4adb8c2b03d3e2565fd376cadae75a639b1cc46c
dochi486 <93mskim@gmail.com>
2022-09-14 23:00:46 +0900

    장바구니에 담긴 아이템 출력 형식 변경

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,31 @@
+import { useContext } from 'react';
+
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount}원`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Schnitzel', amount: 2, price: 28000 }].map(
-        (item) => (
-          <li>{item.name}</li>
-        )
-      )}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amoutn}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
     </ul>
   );
 
@@ -17,13 +34,13 @@ const Cart = (props) => {
       {cartItems}
       <div>
         <span>총 합계</span>
-        <span>36000</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           닫기
         </button>
-        <button className={classes.button}>주문</button>
+        {hasItems && <button className={classes.button}>주문</button>}
       </div>
     </Modal>
   );
