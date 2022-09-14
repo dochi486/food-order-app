1c5cc25e4be9bed9ef2336bd15af837c47e78ad8
dochi486 <93mskim@gmail.com>
2022-09-14 16:37:07 +0900

    card로 음식 목록 감쌈

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Fragment } from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Card from './components/UI/Card';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Card>
+          <Meals />
+        </Card>
+      </main>
     </Fragment>
   );
 }
