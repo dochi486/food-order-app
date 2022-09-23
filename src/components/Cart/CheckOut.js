4e3289825ddc17c320b2c267a0fa42bf36073266
dochi486 <93mskim@gmail.com>
2022-09-23 17:13:53 +0900

    form 유효성 검사 추가

--- src/components/Cart/CheckOut.js
+++ src/components/Cart/CheckOut.js
@@ -1,23 +1,73 @@
 import classes from './CheckOut.module.css';
+import { useRef, useState } from 'react';
+
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim().length === 5;
 
 const CheckOut = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    address: true,
+    postalCode: true,
+  });
+
+  const nameInputRef = useRef();
+  const addressInputRef = useRef();
+  const postalInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredPostalCode = postalInputRef.current.value;
+
+    const enteredNameisValid = !isEmpty(enteredName);
+    const enteredAddressIsValid = !isEmpty(enteredAddress);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+
+    setFormInputsValidity({
+      name: enteredNameisValid,
+      address: enteredAddressIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameisValid && enteredAddressIsValid && enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
   };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputsValidity.name ? '' : classes.invalid
+  }`;
+  const addressControlClasses = `${classes.control} ${
+    formInputsValidity.address ? '' : classes.invalid
+  }`;
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputsValidity.postalCode ? '' : classes.invalid
+  }`;
 
   return (
     <form onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameControlClasses}>
         <label htmlFor="name">받는 분 성함</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>정확한 성함을 입력해주세요.</p>}
       </div>
-      <div className={classes.control}>
+      <div className={addressControlClasses}>
         <label htmlFor="address">주소</label>
-        <input type="text" id="address" />
+        <input type="text" id="address" ref={addressInputRef} />
+        {!formInputsValidity.address && <p>정확한 주소를 입력해주세요.</p>}
       </div>
-      <div className={classes.control}>
+      <div className={postalCodeControlClasses}>
         <label htmlFor="postal">우편번호</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalInputRef} />
+        {!formInputsValidity.postalCode && (
+          <p>5자리의 우편번호를 입력해주세요.</p>
+        )}
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
