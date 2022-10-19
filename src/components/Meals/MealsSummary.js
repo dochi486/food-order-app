4604713de6b3e7caa2cc505eaf586e11a63ac135
dochi486 <93mskim@gmail.com>
2022-10-19 10:57:57 +0900

    문구 변경

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,15 +3,8 @@ import classes from './MealsSummary.module.css';
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>배달 가능한 맛있는 음식들</h2>
-      <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p>
+      <h2>배달 가능한 맛있는 요리들</h2>
+      <p>모든 음식은 좋은 재료와 정성을 들여 만듭니다!</p>
     </section>
   );
 };
